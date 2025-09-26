b3a23c9fb8360690fc36c3d4b59b585e1d0fdc38
Darion Dawson <ddawson3@xula.edu>
2025-09-25 21:10:35 -0500

    coding first three odd problems to driver

--- Driver.java
+++ Driver.java
@@ -1,5 +1,8 @@
 public class Driver {
         public static void main(String[] args) {
+                System.out.println("1. Write code that stores the first N records of the dataset in some custom object.");
+                System.out.println("3. Write code that displays a horizontal text-based histogram of the ages. Remember, you can't use any plotting libraries.");
+                System.out.println("5. Write code that determines the total number of records for each number of children.");
             
         }
 }
