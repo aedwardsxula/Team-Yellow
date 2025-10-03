f8d1ca16b15f75317086edbb7342e5ed1cff7931
Darion Dawson <ddawson3@xula.edu>
2025-10-03 14:09:52 -0500

    add InsuranceDataSource.java class

---
+++ Medical Insurance Data/insurance/InsuranceDataSource.java
@@ -0,0 +1,10 @@
+package com.example.insurance.io;
+
+import com.example.insurance.model.InsuranceRecord;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.List;
+
+public interface InsuranceDataSource {
+    List<InsuranceRecord> readFirstN(Path csvPath, int n) throws IOException;
+}
